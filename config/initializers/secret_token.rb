a1178b69b6875c92f7f89a8ba84bdda051689df9
Dan Ubilla <dan.ubilla@gmail.com>
2012-12-29 20:47:42 -0500

    rails + readme

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fandom::Application.config.secret_token = '4f71c9175b6053edaa45287031e04a8f556f793b23f90baf41faa6a2f3e4214f3be5e43fcd15fc274f7a9d33b3150e162b3a02d5b96116fb90f6107dab166ceb'
