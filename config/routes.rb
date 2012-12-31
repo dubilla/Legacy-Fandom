bb13bfbc9cf6d374c8326619f1f880b1d3ab932a
Dan Ubilla <dan.ubilla@gmail.com>
2012-12-31 00:30:31 -0500

    adding users via devise; switching db to pg

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Fandom::Application.routes.draw do
+
+  get "users/show"
+
+  get "main/index"
+
+  devise_for :users
+
+  match 'users/:id' => 'users#show'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +57,7 @@ Fandom::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'main#index'
 
   # See how all your routes lay out with "rake routes"
 
